015b03eda3e034a612964782135637eda7a4fea4
Daniel <daniel.de.la.garza@hp.com>
2022-07-07 21:43:11 -0700

    add procedrual walk animation

--- Assets/Daniel/Scripts/ProceduralAnimationScript.cs
+++ Assets/Daniel/Scripts/ProceduralAnimationScript.cs
@@ -5,31 +5,69 @@ using UnityEngine;
 public class ProceduralAnimationScript : MonoBehaviour
 {
     [SerializeField] LayerMask raycastLayer = default;
-    [SerializeField] private Transform raycastSource;
-    private Vector3 _oldPosition;
-    
-    
-    
+    [SerializeField] float offsetZ = 0;
+    [SerializeField] float stepDistance = 0.2f;
+    [SerializeField] float stepHeight = 0.2f;
+    [SerializeField] float stepDuration = 0.2f;
+    [SerializeField] ProceduralAnimationScript oppositeLeg = null;
+    private Transform rayCastSource;
+    private Vector3 _oldPosition, _currentPosition, _targetPosition;
+    private float lerp, lerpTime;
+
+    //Set initial IK target position to ground at z offset from source
     private void Start()
     {
-        Ray ray = new Ray(raycastSource.position, Vector3.down);
+        lerp = 1;
+        lerpTime = stepDuration;
+        rayCastSource = transform.parent.transform.Find("raycast_source").transform;
+        Vector3 initalRayCastSource = new Vector3(transform.position.x, 0.08f, transform.position.z);
+        Ray ray = new Ray(initalRayCastSource, Vector3.down);
 
         if (Physics.Raycast(ray, out RaycastHit info, 10, raycastLayer.value))
         {
-            _oldPosition = info.point;
+            _oldPosition = _currentPosition = info.point + Vector3.forward*offsetZ;
+        }
+    }
+    
+    void Update()
+    {
+        transform.position = _currentPosition;
+        Ray ray = new Ray(rayCastSource.position, Vector3.down);
+
+        if (Physics.Raycast(ray, out RaycastHit info, 10, raycastLayer.value))
+        {
+            _targetPosition = info.point;
+            
+            if (Vector3.Distance(_currentPosition, _targetPosition) > stepDistance && lerp >= 1 && oppositeLeg.isGrounded())
+            {
+                lerpTime = 0f;
+            }
+        }
+        
+        lerp = lerpTime/stepDuration;
+        
+        if (lerp < 1)
+        {
+            Vector3 tempPosition = Vector3.Lerp(_oldPosition, _targetPosition, lerp);
+            tempPosition.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
+            
+            _currentPosition = tempPosition;
+            lerpTime += Time.deltaTime;
+        }
+        else
+        {
+            _oldPosition = _currentPosition;
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = _oldPosition;
-    }
-    
     private void OnDrawGizmos()
     {
-
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(_oldPosition, 0.02f);
+        Gizmos.DrawSphere(_targetPosition, 0.02f);
+        Gizmos.DrawLine(_oldPosition, _targetPosition);
+    }
+    private bool isGrounded()
+    {
+        return lerp >= 1;
     }
 }
