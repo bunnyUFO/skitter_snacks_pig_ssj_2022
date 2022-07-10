5f1dfa88398c4d6edd8b0bbc1dcff401d6e7a06f
Daniel <daniel.de.la.garza@hp.com>
2022-07-10 11:54:33 -0700

    add speed to spider controller

--- Assets/Daniel/Scripts/SpiderController.cs
+++ Assets/Daniel/Scripts/SpiderController.cs
@@ -18,34 +18,12 @@ namespace StarterAssets
         [Tooltip("Move speed of the character in m/s")]
         public float MoveSpeed = 2.0f;
 
-        [Tooltip("Sprint speed of the character in m/s")]
-        public float SprintSpeed = 5.335f;
-
-        [Tooltip("How fast the character turns to face movement direction")]
-        [Range(0.0f, 0.3f)]
-        public float RotationSmoothTime = 0.12f;
-
-        [Tooltip("Acceleration and deceleration")]
-        public float SpeedChangeRate = 10.0f;
-
-        // player
-        private float _speed;
-        private float _targetRotation = 0.0f;
-        private float _rotationVelocity;
-
-        // timeout deltatime
-        private float _jumpTimeoutDelta;
-        private float _fallTimeoutDelta;
 
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
         private PlayerInput _playerInput;
 #endif
-        private Animator _animator;
         private Rigidbody _rigidbody;
         private StarterAssetsInputs _input;
-        private GameObject _mainCamera;
-
-        private const float _threshold = 0.01f;
         private bool IsCurrentDeviceMouse
         {
             get
@@ -58,15 +36,6 @@ namespace StarterAssets
             }
         }
 
-        private void Awake()
-        {
-            // get a reference to our main camera
-            if (_mainCamera == null)
-            {
-                _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
-            }
-        }
-        
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
@@ -85,72 +54,20 @@ namespace StarterAssets
 
         private void Move(float time)
         {
-            //Commented code block is to set speed acceleration
-            //Currently move speed it just a normalized vector (speed of 1)
-            
-            // set target speed based on move speed, sprint speed and if sprint is pressed
-            float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
-
-            // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
-
-            // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-            // if there is no input, set the target speed to 0
-            if (_input.move == Vector2.zero) targetSpeed = 0.0f;
-
-            // a reference to the players current horizontal velocity
-            float currentHorizontalSpeed = new Vector3(_rigidbody.velocity.x, 0.0f, _rigidbody.velocity.z).magnitude;
-
-            float speedOffset = 0.1f;
-            float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
-            
-            // accelerate or decelerate to target speed
-            if (currentHorizontalSpeed < targetSpeed - speedOffset ||
-                currentHorizontalSpeed > targetSpeed + speedOffset)
-            {
-                // creates curved result rather than a linear one giving a more organic speed change
-                // note T in Lerp is clamped, so we don't need to clamp our speed
-                _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
-                    Time.deltaTime * SpeedChangeRate);
-
-                // round speed to 3 decimal places
-                _speed = Mathf.Round(_speed * 1000f) / 1000f;
-            }
-            else
-            {
-                _speed = targetSpeed;
-            }
-            
-            
             Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
-            
-            
-             // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-            // if there is a move input rotate player when the player is moving
-            if (_input.move != Vector2.zero)
-            {
-                _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
-                                  _mainCamera.transform.eulerAngles.y;
-                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
-                    RotationSmoothTime);
-            
-                // rotate to face input direction relative to camera position
-                // TODO fix rotation to not override rotation from Spider.cs
-                // transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
-            }
-            
-            if (Keyboard.current.qKey.isPressed)
+
+            if (Keyboard.current.qKey.isPressed || Mouse.current.leftButton.isPressed)
             {
                 transform.Rotate(0f, -360f*time, 0f);
             }
-            if (Keyboard.current.eKey.isPressed)
+            if (Keyboard.current.eKey.isPressed || Mouse.current.rightButton.isPressed)
             {
                 transform.Rotate(0f, 360*time, 0f);
             }
 
-
             // move the player
             Vector3 targetDirection = transform.forward * inputDirection.z + transform.right * inputDirection.x;
-            _rigidbody.velocity = (targetDirection.normalized);
+            _rigidbody.velocity = (targetDirection.normalized)*MoveSpeed;
         }
     }
 }
