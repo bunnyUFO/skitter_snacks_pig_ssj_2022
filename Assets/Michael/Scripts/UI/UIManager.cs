613f08c0839c895f305eec1c63e71bd9620515bb
Michael-Ennis-1 <1902141@uad.ac.uk>
2022-07-11 19:12:22 +0100

    Day 5 Work
    Debug testing, food system implemented

--- Assets/Michael/Scripts/UI/UIManager.cs
+++ Assets/Michael/Scripts/UI/UIManager.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Text text;
+    string foodString;
+
+    [Header("Found Food")]
+    public List<Food> foodList = new List<Food>();
+
+    [Header("Food Collected")]
+    public int food;
+
     void Start()
     {
-        
+        text = GetComponentInChildren<Text>();
+
+        foreach (GameObject food in GameObject.FindGameObjectsWithTag("Food"))
+        {
+            foodList.Add(food.GetComponent<Food>());
+        }
+
+        foodString = "Food: " + food + "/" + foodList.Count;
+        text.text = foodString;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < foodList.Count; i++)
+        {
+            if (foodList[i].Collected && !foodList[i].Sent)
+            {
+                UpdateText();
+                foodList[i].Sent = true;
+            }
+        }
+    }
+
+    void UpdateText()
+    {
+        food++;
+
+        foodString = "Food: " + food + "/" + foodList.Count;
+        text.text = foodString;
     }
 }
