603d3b40c263a723caf3a148ef5e9ad20aeeed9c
Daniel <daniel.de.la.garza@hp.com>
2022-07-12 08:43:11 -0700

    fix ant rotation

--- Assets/Michael/Scripts/Ant.cs
+++ Assets/Michael/Scripts/Ant.cs
@@ -1,86 +1,69 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ant : MonoBehaviour
 {
-    [Header("Body Positioning")]
-    [LabelOverride( "Body Y Offset" )]
-    [SerializeField] float offsetY = 0.2f;
-    [LabelOverride( "Rotation Curve" )]
-    [SerializeField] AnimationCurve sensitivityCurve;
-    [SerializeField] private List<ProceduralAnimationScript> legs;
-    private Rigidbody _rigidbody;
+    [Header("Body Positioning")] [LabelOverride("Body Y Offset")] [SerializeField]
+    float offsetY = 0.2f;
 
-    private void Awake()
-    {
-        _rigidbody = transform.GetComponent<Rigidbody>();
-    }
+    [LabelOverride("Rotation Speed Curve")] [SerializeField]
+    AnimationCurve sensitivityCurve;
+
+    [SerializeField] List<ProceduralAnimationScript> legs;
+    [SerializeField] bool debug = false;
 
     void Update()
     {
-        // update all leg positions
-        foreach(ProceduralAnimationScript leg in legs)
+        foreach (ProceduralAnimationScript leg in legs)
         {
             leg.UpdatePosition(Time.deltaTime);
         }
-
         CalculateOrientation();
     }
-    
-    private Vector3 GetMeanLegPosition(){
-        if (legs.Count == 0)
-            return Vector3.zero;
-         
-        float x = 0f;
-        float y = 0f;
-        float z = 0f;
- 
-        foreach (ProceduralAnimationScript leg in legs)
-        {
-            Vector3 pos = leg.GetOldPosition();
-            x += pos.x;
-            y += pos.y;
-            z += pos.z;
-        }
-        return new Vector3(x / legs.Count, y / legs.Count, z / legs.Count);
-    }
 
-    private void CalculateOrientation() {
+    private void CalculateOrientation()
+    {
         Vector3 up = Vector3.zero;
         float avgSurfaceDist = 0;
+        Vector3 a, b, c, oldPoint, targetPoint;
 
-        Vector3 point, a, b, c;
-
-        // cross product of adjacent leg pairs to calculate average up
         for (int i = 0; i < legs.Count; i++)
         {
+            // Calculate adjacent leg distance from body
             ProceduralAnimationScript legPair = i == 0 ? legs[legs.Count - 1] : legs[i - 1];
-            point = legs[i].GetOldPosition();
-            avgSurfaceDist += transform.InverseTransformPoint(point).y;
-            a = (transform.position - point).normalized;
-            b = (legPair.GetOldPosition() - point).normalized;
-            c = Vector3.Cross(a, b);
-            up += c * sensitivityCurve.Evaluate(c.magnitude) + (legs[i].stepNormal == Vector3.zero ? transform.forward : legs[i].stepNormal);
+            oldPoint = legs[i].GetOldPosition();
+            targetPoint = legs[i].GetTargetPosition();
+            avgSurfaceDist += transform.InverseTransformPoint(targetPoint).y;
+            a = (transform.position - oldPoint).normalized;
+            b = (legPair.GetOldPosition() - oldPoint).normalized;
 
-            Debug.DrawRay(point, c, Color.yellow, 0);
-            Debug.DrawRay(point, legs[i].stepNormal, Color.magenta, 0);
-            
+            // Calculate product of adjacent leg distance from body
+            c = Vector3.Cross(a, b);
+
+            // Use leg surface normal vectors and leg distance cross products to calculate body up vector
+            // up += (legs[i].stepNormal == Vector3.zero ? transform.forward : legs[i].stepNormal);
+            up += c * sensitivityCurve.Evaluate(c.magnitude) +
+                  (legs[i].stepNormal == Vector3.zero ? transform.up : legs[i].stepNormal);
+
+            if (debug)
+            {
+                Debug.DrawRay(oldPoint, c, Color.yellow, 0);
+                Debug.DrawRay(oldPoint, legs[i].stepNormal, Color.magenta, 0);
+            }
         }
+
         up /= legs.Count;
         avgSurfaceDist /= legs.Count;
-        
-        Debug.DrawRay(transform.position, up, Color.green, 0);
+        if (debug)
+        {
+            Debug.DrawRay(transform.position, up, Color.green, 0);
+        }
+
+
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+            Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, up), up), 22.5f * Time.deltaTime);
         transform.Translate(0, -(-avgSurfaceDist + -offsetY) * 0.5f, 0, Space.Self);
     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(transform.position, 0.1f);
-        Gizmos.color = Color.red;
-    }
-
-
 }
