b9c1bd6b2688599c56288c6f73fb09d68226955d
Michael-Ennis-1 <1902141@uad.ac.uk>
2022-07-12 16:11:20 +0100

    Day 6
    Start

--- Assets/AntController.cs
+++ Assets/AntController.cs
@@ -1,23 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AntController : MonoBehaviour
 {
     Rigidbody _rigidbody;
-
     Vector3 targetDirection;
 
-    public float MoveSpeed = 2.0f;
+    NavMeshAgent agent;
+
+    // Need to turn off Navmesh Agent, then Disable kinematics, then Move Forward X amount of time/until at the end point, then Renable Kinematics and Navmesh Agent and end the navlink
+
+    public float transitionSpeed = 3.0f;
 
     void Start()
     {
         _rigidbody = transform.GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoTraverseOffMeshLink = false;
     }
 
     void Update()
     {
-        // targetDirection = transform.forward;
-        // _rigidbody.velocity = targetDirection * MoveSpeed;
+        
+        //if (agent.isOnOffMeshLink)
+        //{
+            agent.updatePosition = false;
+            agent.updateRotation = false;
+            targetDirection = transform.forward;
+            _rigidbody.velocity = targetDirection * transitionSpeed;
+
+        // Get on Y axis to determine which direction it is going in, if omni directional will need to redo, then Complete Link
+            if (this.transform.position == agent.currentOffMeshLinkData.endPos)
+            {
+
+            }
+        //}
     }
 }
