2cb3de1f385f3f53d26b37fa96c09de3e1f7f972
Michael-Ennis-1 <1902141@uad.ac.uk>
2022-07-14 19:37:04 +0100

    Day 8
    Developed mid point strategy for manual mesh link

--- Assets/ManualLinkV4.cs
+++ Assets/ManualLinkV4.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ManualLinkV3 : MonoBehaviour
+public class ManualLinkV4 : MonoBehaviour
 {
     NavMeshAgent _agent;
     Rigidbody _rigidBody;
@@ -13,7 +13,8 @@ public class ManualLinkV3 : MonoBehaviour
 
     Vector3 linkEndPoint;
     Vector3 linkStartPoint;
-    Vector3 linkRotationPoint;
+    //Vector3 linkRotationPoint;
+    Vector3 linkMidRotationPoint;
 
     Vector3 realStartPoint;
     Vector3 realEndPoint;
@@ -36,13 +37,78 @@ public class ManualLinkV3 : MonoBehaviour
 
     void Update()
     {
-        // Obtain the Correct Rotation point
-        if (_agent.nextOffMeshLinkData.startPos != new Vector3 (0, 0, 0) && !traversing)
+        
+        if (_agent.nextOffMeshLinkData.startPos != new Vector3(0, 0, 0) && !traversing)
+        {
+            linkStartPoint = _agent.nextOffMeshLinkData.startPos;
+            linkEndPoint = _agent.nextOffMeshLinkData.endPos;
+            //_agent.nextOffMeshLinkData
+
+            distanceFromStart = (linkStartPoint - this.transform.position).magnitude;
+
+            linkMidRotationPoint.x = linkStartPoint.x + (linkEndPoint.x - linkStartPoint.x) / 2;
+            linkMidRotationPoint.y = linkStartPoint.y + (linkEndPoint.y - linkStartPoint.y) / 2;
+            linkMidRotationPoint.z = linkStartPoint.z + (linkEndPoint.z - linkStartPoint.z) / 2;
+
+            if (distanceFromStart < 2f)
+            {
+                _agent.updatePosition = false;
+                _agent.updateRotation = false;
+                _agent.updateUpAxis = false;
+
+                traversing = true;
+            }
+        }
+
+        if (traversing)
+        {
+            Debug.Log("Is Traversing");
+
+            _rigidBody.velocity = this.transform.forward * 3.0f;
+
+            direction = (linkMidRotationPoint - transform.position).normalized;
+
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, direction.y, direction.z));
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 8 * Time.deltaTime);
+
+            distanceFromMid = (linkMidRotationPoint - this.transform.position).magnitude;
+            distanceFromEnd = (linkEndPoint - this.transform.position).magnitude;
+            Debug.Log("Distance from Mid" + distanceFromMid);
+            Debug.Log("Distance from End" + distanceFromEnd);
+
+            if (distanceFromMid < 0.5f)
+            {
+                Debug.Log("Has Reached Mid");
+                linkMidRotationPoint = linkEndPoint;
+            }
+            if (distanceFromEnd < 0.5f)
+            {
+                Debug.Log("Has Reached End");
+                _rigidBody.velocity = this.transform.forward * 0;
+
+                _agent.updatePosition = true;
+                _agent.updateRotation = true;
+                _agent.updateUpAxis = true;
+
+                _agent.CompleteOffMeshLink();
+
+                traversing = false;
+            }
+        }
+
+        /*
+        // Obtain the triangular midpoint of a navlink
+        if (_agent.nextOffMeshLinkData.startPos != new Vector3(0, 0, 0) && !traversing)
         {
             linkStartPoint = _agent.nextOffMeshLinkData.startPos;
             linkEndPoint = _agent.nextOffMeshLinkData.endPos;
 
             distanceFromStart = (linkStartPoint - this.transform.position).magnitude;
+
+            linkMidRotationPoint.x = linkStartPoint.x + (linkEndPoint.x - linkStartPoint.x) / 2;
+            linkMidRotationPoint.y = linkStartPoint.y + (linkEndPoint.y - linkStartPoint.y) / 2;
+            linkMidRotationPoint.z = linkStartPoint.z + (linkEndPoint.z - linkStartPoint.z) / 2;
 
             Debug.Log("Distance from start: " + distanceFromStart);
 
@@ -50,7 +116,7 @@ public class ManualLinkV3 : MonoBehaviour
             if (distanceFromStart < 2f)
             {
                 _agent.updatePosition = false;
-                //_agent.updateRotation = false;
+                _agent.updateRotation = false;
                 _agent.updateUpAxis = false;
 
                 traversing = true;
@@ -64,27 +130,27 @@ public class ManualLinkV3 : MonoBehaviour
                 {
                     if (linkStartPoint.z < linkEndPoint.z) // Going Up on X Axis
                     {
-                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y + 0.2f, linkStartPoint.z + 0.2f);
+                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y, linkStartPoint.z + 0.2f);
                         linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y, linkStartPoint.z + 0.2f);
-                    } 
+                    }
                     else // Going Down on X Axis
                     {
-                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y + 0.2f, linkStartPoint.z - 0.2f);
+                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y, linkStartPoint.z - 0.2f);
                         linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y, linkStartPoint.z - 0.2f);
                     }
                 }
-                
+
                 // If also on Z axis
                 if (linkStartPoint.z == linkEndPoint.z)
                 {
                     if (linkStartPoint.x < linkEndPoint.x) // Going Up on Z Axis
                     {
-                        //linkRotationPoint = new Vector3(linkStartPoint.x + 0.2f, linkEndPoint.y + 0.2f, linkStartPoint.z);
+                        //linkRotationPoint = new Vector3(linkStartPoint.x + 0.2f, linkEndPoint.y, linkStartPoint.z);
                         linkRotationPoint = new Vector3(linkStartPoint.x + 0.2f, linkEndPoint.y, linkStartPoint.z);
                     }
                     else // Going Down on Z Axis
                     {
-                        //linkRotationPoint = new Vector3(linkStartPoint.x - 0.2f, linkEndPoint.y + 0.2f, linkStartPoint.z);
+                        //linkRotationPoint = new Vector3(linkStartPoint.x - 0.2f, linkEndPoint.y, linkStartPoint.z);
                         linkRotationPoint = new Vector3(linkStartPoint.x - 0.2f, linkEndPoint.y, linkStartPoint.z);
                     }
                 }
@@ -98,12 +164,12 @@ public class ManualLinkV3 : MonoBehaviour
                 {
                     if (linkStartPoint.z < linkEndPoint.z) // Going Up on X Axis
                     {
-                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y + 0.2f, linkStartPoint.z + 0.2f);
+                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y + 0.2f, linkStartPoint.z);
                         linkRotationPoint = new Vector3(linkStartPoint.x, linkStartPoint.y + 0.2f, linkEndPoint.z);
                     }
                     else // Going Down on X Axis
                     {
-                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y + 0.2f, linkStartPoint.z - 0.2f);
+                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y + 0.2f, linkStartPoint.z);
                         linkRotationPoint = new Vector3(linkStartPoint.x, linkStartPoint.y + 0.2f, linkEndPoint.z);
                     }
                 }
@@ -113,12 +179,12 @@ public class ManualLinkV3 : MonoBehaviour
                 {
                     if (linkStartPoint.x < linkEndPoint.x) // Going Up on Z Axis
                     {
-                        //linkRotationPoint = new Vector3(linkStartPoint.x + 0.2f, linkEndPoint.y + 0.2f, linkStartPoint.z);
+                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y + 0.2f, linkStartPoint.z);
                         linkRotationPoint = new Vector3(linkEndPoint.x, linkStartPoint.y + 0.2f, linkEndPoint.z);
                     }
                     else // Going Down on Z Axis
                     {
-                        //linkRotationPoint = new Vector3(linkStartPoint.x - 0.2f, linkEndPoint.y + 0.2f, linkStartPoint.z);
+                        //linkRotationPoint = new Vector3(linkStartPoint.x, linkEndPoint.y + 0.2f, linkStartPoint.z);
                         linkRotationPoint = new Vector3(linkEndPoint.x, linkStartPoint.y + 0.2f, linkEndPoint.z);
                     }
                 }
@@ -153,23 +219,29 @@ public class ManualLinkV3 : MonoBehaviour
                 _rigidBody.velocity = this.transform.forward * 0;
 
                 _agent.updatePosition = true;
-                //_agent.updateRotation = true;
+                _agent.updateRotation = true;
                 _agent.updateUpAxis = true;
 
                 _agent.CompleteOffMeshLink();
 
                 traversing = false;
             }
-        }   
+        }
+        */
     }
+        
 
     private void OnDrawGizmos()
     {
+        //Gizmos.color = Color.green;
+        //Gizmos.DrawSphere(new Vector3(_agent.currentOffMeshLinkData.end))
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(linkEndPoint, 0.2f);
         Gizmos.color = Color.green;
         Gizmos.DrawSphere(linkStartPoint, 0.2f);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(linkRotationPoint, 0.2f);
+        //Gizmos.color = Color.yellow;
+        //Gizmos.DrawSphere(linkRotationPoint, 0.2f);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(linkMidRotationPoint, 0.2f);
     }
 }
